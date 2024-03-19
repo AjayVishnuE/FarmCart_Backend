053f5d0909ae8522b5c525fdaa80bcd59d5d6ba5
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-03-19 22:40:03 +0530

    host configurations

--- FarmCart_Backend/settings.py
+++ FarmCart_Backend/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
