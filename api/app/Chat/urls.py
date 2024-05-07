1c5b22570ec269e9ff4b9fc96506b4cd5f86ad3d
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-05-07 20:41:27 +0530

    requirement update

--- api/app/Chat/urls.py
+++ api/app/Chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .chats_views import ChatbotView, ComplaintAPIView, ChatGPTView
+from .chats_views import ChatbotView, ComplaintAPIView
 
 urlpatterns = [
     path('chat/', ChatbotView.as_view(), name='chatbot'),
-    path('chatgpt/', ChatGPTView.as_view(), name='chatbot'),
     path('complaint/', ComplaintAPIView.as_view(), name='complaint-api'),
+    # path('chatgpt/', ChatGPTView.as_view(), name='chatbot'),
 ]
