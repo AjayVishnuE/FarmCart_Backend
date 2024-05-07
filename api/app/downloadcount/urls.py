9366df2233111b9431b19dcdec44de0b7b53d69d
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-05-08 01:43:40 +0530

    patch download count

--- api/app/downloadcount/urls.py
+++ api/app/downloadcount/urls.py
@@ -3,4 +3,5 @@ from .downloadcounter_views import DownloadCountView
 
 urlpatterns = [
     path('getpost/', DownloadCountView.as_view(), name='download-count'),
+    path('download-count/<uuid:pk>/', DownloadCountView.as_view()),
 ]
